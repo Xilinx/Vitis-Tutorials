6128e43e0bfa9e8cf476e256e0d7656f2892ecd0
Dachang Li <dachang.li@xilinx.com>
2020-06-02 07:32:42 +0800

    Create V4l2Access.hpp

---
+++ Machine_Learning_Tutorial/Section_3-Basic/Module_6/myV4L2/include/V4l2Access.hpp
@@ -0,0 +1,38 @@
+#ifndef V4L2_ACCESS
+#define V4L2_ACCESS
+
+#include "V4l2Device.h"
+
+class V4l2Access
+{
+	public:
+		enum IoType
+		{
+			IOTYPE_READWRITE,
+			IOTYPE_MMAP
+		};
+		
+		V4l2Access(V4l2Device* device);
+		virtual ~V4l2Access();
+		
+		int getFd()         { return m_device->getFd();         }
+		unsigned int getBufferSize() { return m_device->getBufferSize(); }
+		unsigned int getFormat()     { return m_device->getFormat();     }
+		unsigned int getWidth()      { return m_device->getWidth();      }
+		unsigned int getHeight()     { return m_device->getHeight();     }
+		void queryFormat()  { m_device->queryFormat();          }
+
+		int isReady()       { return m_device->isReady();       }
+		int start()         { return m_device->start();         }
+		int stop()          { return m_device->stop();          }
+
+	private:
+		V4l2Access(const V4l2Access&);
+		V4l2Access & operator=(const V4l2Access&);
+	
+	protected:
+		V4l2Device* m_device;		
+};
+
+
+#endif
