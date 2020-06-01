99248a581824100de16358f0c733b0335f47d92f
Dachang Li <dachang.li@xilinx.com>
2020-06-02 07:36:49 +0800

    Create V4l2ReadWriteDevice.hpp

---
+++ Machine_Learning_Tutorial/Section_3-Basic/Module_6/myV4L2/include/V4l2ReadWriteDevice.hpp
@@ -0,0 +1,19 @@
+#ifndef V4L2_RW_DEVICE
+#define V4L2_RW_DEVICE
+ 
+#include "V4l2Device.h"
+
+
+class V4l2ReadWriteDevice : public V4l2Device
+{	
+	protected:	
+		virtual size_t writeInternal(char* buffer, size_t bufferSize);
+		virtual size_t readInternal(char* buffer, size_t bufferSize);
+		
+	public:
+		V4l2ReadWriteDevice(const V4L2DeviceParameters&  params, v4l2_buf_type deviceType);
+};
+
+
+#endif
+
