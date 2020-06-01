4c0184a22bcdba7a815290a57ab1c9f73ca23037
Dachang Li <dachang.li@xilinx.com>
2020-06-02 07:34:48 +0800

    Create V4l2Output.hpp

---
+++ Machine_Learning_Tutorial/Section_3-Basic/Module_6/myV4L2/include/V4l2Output.hpp
@@ -0,0 +1,25 @@
+#ifndef V4L2_OUTPUT
+#define V4L2_OUTPUT
+
+#include "V4l2Access.h"
+
+// ---------------------------------
+// V4L2 Output
+// ---------------------------------
+class V4l2Output : public V4l2Access
+{		
+	protected:
+		V4l2Output(V4l2Device* device);
+
+	public:
+		static V4l2Output* create(const V4L2DeviceParameters & param, IoType iotype = V4l2Access::IOTYPE_MMAP);
+		~V4l2Output();
+	
+		size_t write(char* buffer, size_t bufferSize);
+		int    isWritable(timeval* tv);
+		bool   startPartialWrite(void);
+		size_t writePartial(char* buffer, size_t bufferSize);
+		bool   endPartialWrite(void);
+};
+
+#endif
