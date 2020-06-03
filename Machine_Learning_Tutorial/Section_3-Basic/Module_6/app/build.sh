beccce2fa8ec53f60cd28988bcbdd4597e8a17f8
Dachang Li <dachang.li@xilinx.com>
2020-06-03 19:42:27 +0800

    Create build.sh

---
+++ Machine_Learning_Tutorial/Section_3-Basic/Module_6/app/build.sh
@@ -0,0 +1,2 @@
+export CPLUS_INCLUDE_PATH=/usr/include/drm/:$CPLUS_INCLUDE_PATH
+g++ -std=c++14 -O2 -o usb_input_multi_threads_refinedet_drm test/usb_input_multi_threads_refinedet.cpp -Iinclude src/*.cpp -lglog -lmy_v4l2s -lopencv_core -lopencv_video -lopencv_videoio -lopencv_imgproc -lopencv_imgcodecs -lopencv_highgui -lvitis_ai_library-refinedet -lvitis_ai_library-model_config -ljson-c -lglog -lpthread -ldrm -lvart-runner -DUSE_DRM=1
