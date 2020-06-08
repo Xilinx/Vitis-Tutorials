11bedfe2468dd9a715acba795ec2af9107f486ef
Dachang Li <dachang.li@xilinx.com>
2020-06-08 13:51:04 +0800

    License update and add comment

--- Machine_Learning_Tutorial/Section_3-Basic/Module_6/app/include/guithread.hpp
+++ Machine_Learning_Tutorial/Section_3-Basic/Module_6/app/include/guithread.hpp
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2019 Xilinx Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 #ifndef GUITHREAD
 #define GUITHREAD
 #include "frameinfo.hpp"
@@ -20,8 +37,18 @@
 namespace vitis{
     namespace ai{
 using queue_t = vitis::ai::BoundedQueue<FrameInfo>;
-
+/**
+ * @struct  GuiThread
+ * 
+ * @brief   display thread
+ * 
+ */
 struct GuiThread : public MyThread {
+  /**
+   * @brief create an instance of GuiThread
+   * 
+   * @return std::shared_ptr<GuiThread> 
+   */
   static std::shared_ptr<GuiThread> instance();
   GuiThread();
   virtual ~GuiThread() {  //
@@ -29,22 +56,65 @@ struct GuiThread : public MyThread {
     vitis::ai::imshow_close();
 #endif
   }
+  /**
+   * @brief clean up the queue set the dirty flag of the FrameInfo true.
+   * 
+   */
   void clean_up_queue();
+
+  /**
+   * @brief run the display function
+   * 
+   * @return int 
+   */
   virtual int run();
 
+/**
+ * @brief naming a Gui thread
+ * 
+ * @return std::string 
+ */
   virtual std::string name() override; 
 
+  /**
+   * @brief get queue function, the display frame need to be copy the this queue.
+   * 
+   * @return queue_t* 
+   */
   queue_t* getQueue(); 
 
+  /**
+   * @brief the FrameInfo queue to display
+   * 
+   */
   std::unique_ptr<queue_t> queue_;
+  /**
+   * @brief setting the time out counter
+   * 
+   */
   int inactive_counter_;
   
+  /**
+   * @brief add dirty flag to FrameInfo
+   * 
+   */
   struct FrameCache {
     bool dirty;
     FrameInfo frame_info;
   };
  std::map<int, FrameCache> frames_;
+
+
+ /**
+  * @brief display windows for DRM
+  * 
+  */
  static std::vector<cv::Rect> rects;
+
+ /**
+  * @brief backgourd image for DRM
+  * 
+  */
  static cv::Mat img;
 };
 
