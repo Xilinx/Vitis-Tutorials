bf8e4012567e39d74e19131019675985d7e33dec
Shuai Zhang <shuai.zhang@xilinx.com>
2020-06-22 14:32:25 +0800

    Update get_network.sh

--- Machine_Learning_Tutorial/Section_3-Basic/Module_5/get_network.sh
+++ Machine_Learning_Tutorial/Section_3-Basic/Module_5/get_network.sh
@@ -4,9 +4,9 @@ NETWORK_NAME=cf_refinedet_coco_480_360_0.96_1.1.zip
 
 wget -O $NETWORK_NAME  https://www.xilinx.com/bin/public/openDownload?filename=$NETWORK_NAME
 
-#unzip $NETWORK_NAME 
+unzip $NETWORK_NAME 
 
-#rm $NETWORK_NAME
+rm $NETWORK_NAME
 
 mv cf_refinedet_coco_480_360_0.96_5.08G/code cf_refinedet_coco_480_360_0.96_5.08G/code_bak
-cp -r reference_scripts/code refinedet_coco_480_360_0.96_5.08G/
+cp -r reference_scripts/code cf_refinedet_coco_480_360_0.96_5.08G/
