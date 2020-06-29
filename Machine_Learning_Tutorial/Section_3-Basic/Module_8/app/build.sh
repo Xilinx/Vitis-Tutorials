df95808097a111797bc2f359a9dccf91d98022af
Dachang Li <dachang.li@xilinx.com>
2020-06-29 18:17:09 +0800

    Create build.sh
    Former-commit-id: f2d80c0d21e16ba44bd70a97779b4faedc0155ff

---
+++ Machine_Learning_Tutorial/Section_3-Basic/Module_8/app/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+g++ -std=c++14 -O2 -o usb_input_multi_threads_xcl_refinedet_drm test/usb_input_multi_threads_refinedet.cpp -Iinclude src/*.cpp -lglog -lmy_v4l2s -lopencv_core -lopencv_video -lopencv_videoio -lopencv_imgproc -lopencv_imgcodecs -lopencv_highgui -lvitis_ai_library-refinedet -lvitis_ai_library-model_config -ljson-c -lglog -lpthread -ldrm -lvart-runner -lxrt_core -lxrt_coreutil -lxilinxopencl -I/usr/include/xrt -I/usr/include/drm -DUSE_DRM=1
