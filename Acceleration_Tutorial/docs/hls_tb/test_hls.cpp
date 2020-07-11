9778a37c1ffb46e43f21be829b101a1a7bfc8116
Joyce Li - Xilinx <31024466+JoyceLi-Xilinx@users.noreply.github.com>
2020-07-11 13:56:38 +0800

    update source code
    fix format

--- Acceleration_Tutorial/docs/hls_tb/test_hls.cpp
+++ Acceleration_Tutorial/docs/hls_tb/test_hls.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Xilinx, Inc.
+ * Copyright 2020 Xilinx, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,53 +28,52 @@
 //! Core function of Cholesky benchmark
 int main(int argc, const char* argv[]) {
 
-	  const int dataAM = 16, dataAN = 16;
-    std::cout << "INFO: Matrix Row M: " << dataAM << std::endl;
-    std::cout << "INFO: Matrix Col N: " << dataAN << std::endl;
+   const int dataAM = 16, dataAN = 16;
+   std::cout << "INFO: Matrix Row M: " << dataAM << std::endl;
+   std::cout << "INFO: Matrix Col N: " << dataAN << std::endl;
 
-    double dataA[dataAN*dataAM];
-    double dataC[dataAN][dataAM];
+   double dataA[dataAN*dataAM];
+   double dataC[dataAN][dataAM];
 
-    FILE *fp;
-    double tmp;
+   FILE *fp;
+   double tmp;
 
-    fp=fopen("tb_data/matrix_input.dat","r");
-    for (int i = 0; i < dataAN*dataAM; i++){
-    	fscanf(fp, "%lf", &tmp);
-    	dataA[i] = tmp;
-    }
-    fclose(fp);
+   fp=fopen("tb_data/matrix_input.dat","r");
+   for (int i = 0; i < dataAN*dataAM; i++){
+      fscanf(fp, "%lf", &tmp);
+      dataA[i] = tmp;
+   }
+   fclose(fp);
 
-    fp=fopen("tb_data/golden_result.dat","r");
-    for (int i = 0; i < dataAN; i++){
-    	for (int j = 0; j < dataAM; j++){
-    		fscanf(fp, "%lf", &tmp);
-    		dataC[i][j] = tmp;
-    	}
-    }
-    fclose(fp);
+   fp=fopen("tb_data/golden_result.dat","r");
+   for (int i = 0; i < dataAN; i++){
+      for (int j = 0; j < dataAM; j++){
+         fscanf(fp, "%lf", &tmp);
+         dataC[i][j] = tmp;
+      }
+   }
+   fclose(fp);
 
-	  cholesky_kernel(dataAN, dataA);
+   cholesky_kernel(dataAN, dataA);
 
-    // Calculate err between dataA and dataC
-    double errA = 0;
-    for (int i = 0; i < dataAM; i++) {
-        for (int j = 0; j <= i; j++) {
-            errA += (dataA[i * dataAN + j] - dataC[i][j]) * (dataA[i * dataAN + j] - dataC[i][j]);
-
-        }
-    }
-    errA = std::sqrt(errA);
-    std::cout << "errA = " << errA << std::endl;
-
-    std::cout << "-------------- " << std::endl;
-    if (errA > 0.0001) {
-        std::cout << "INFO: Result false" << std::endl;
-        std::cout << "-------------- " << std::endl;
-        return -1;
-    } else {
-        std::cout << "INFO: Result correct" << std::endl;
-        std::cout << "-------------- " << std::endl;
-        return 0;
-    }
+   // Calculate err between dataA and dataC
+   double errA = 0;
+   for (int i = 0; i < dataAM; i++) {
+      for (int j = 0; j <= i; j++) {
+         errA += (dataA[i * dataAN + j] - dataC[i][j]) * (dataA[i * dataAN + j] - dataC[i][j]);
+      }
+   }
+   errA = std::sqrt(errA);
+   std::cout << "errA = " << errA << std::endl;
+   std::cout << "-------------- " << std::endl;
+   if (errA > 0.0001) {
+      std::cout << "INFO: Result false" << std::endl;
+      std::cout << "-------------- " << std::endl;
+      return -1;
+   }
+   else {
+      std::cout << "INFO: Result correct" << std::endl;
+      std::cout << "-------------- " << std::endl;
+      return 0;
+   }
 }
