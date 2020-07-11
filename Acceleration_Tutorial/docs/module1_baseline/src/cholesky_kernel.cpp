7e74364b35ac9557bc04fd755cea90fb6df53091
Joyce Li - Xilinx <31024466+JoyceLi-Xilinx@users.noreply.github.com>
2020-07-11 13:16:37 +0800

    Update cholesky_kernel.cpp

--- Acceleration_Tutorial/docs/module1_baseline/src/cholesky_kernel.cpp
+++ Acceleration_Tutorial/docs/module1_baseline/src/cholesky_kernel.cpp
@@ -18,49 +18,49 @@
 
 extern "C" void cholesky_kernel(int diagSize, double* matrixA) {
 
-  dataType dataA[MAXN][MAXN];
+   dataType dataA[MAXN][MAXN];
 
-	for(int i = 0; i < diagSize; i++){
-		for (int j = 0; j < diagSize; j++) {
-	     dataA[i][j] = matrixA[i*diagSize + j];
-		}
-	}
+   for(int i = 0; i < diagSize; i++){
+      for (int j = 0; j < diagSize; j++) {
+      dataA[i][j] = matrixA[i*diagSize + j];
+      }
+   }
 
-  double tmp1=sqrt(dataA[0][0]);
+   double tmp1=sqrt(dataA[0][0]);
 
-  dataA[0][0] = tmp1;
-  Loop_first_col:
-  for (int i = 1; i < diagSize; i++){
-	   dataA[i][0] = dataA[i][0]/tmp1;
-  }
+   dataA[0][0] = tmp1;
+   Loop_first_col:
+   for (int i = 1; i < diagSize; i++){
+      dataA[i][0] = dataA[i][0]/tmp1;
+   }
 
-  Loop_col:
-  for (int j = 1; j < diagSize; ++j){
-     dataType tmp = 0;
+   Loop_col:
+   for (int j = 1; j < diagSize; ++j){
+      dataType tmp = 0;
 
-	Loop_diag:
-	for(int k = 0; k < j; k++){
-		tmp += dataA[j][k]*dataA[j][k];
-	}
+      Loop_diag:
+      for(int k = 0; k < j; k++){
+         tmp += dataA[j][k]*dataA[j][k];
+      }
 
-	dataA[j][j] = sqrt(dataA[j][j] - tmp);
+      dataA[j][j] = sqrt(dataA[j][j] - tmp);
 
-	if (j < diagSize - 1){
-	Loop_row:
-     for(int i = j+1; i < diagSize; ++i){
-	   dataType tmp2=0;
-	   Loop_vec_mul:
-	   for(int k = 0; k < j; k++){
-        tmp2 += dataA[i][k]*dataA[j][k];
-	   }
-	   dataA[i][j] = (dataA[i][j] - tmp2)/dataA[j][j];
-     }
-	}
-  }
+      if (j < diagSize - 1){
+      Loop_row:
+      for(int i = j+1; i < diagSize; ++i){
+         dataType tmp2=0;
+         Loop_vec_mul:
+         for(int k = 0; k < j; k++){
+            tmp2 += dataA[i][k]*dataA[j][k];
+         }
+         dataA[i][j] = (dataA[i][j] - tmp2)/dataA[j][j];
+      }
+      }
+   }
 
-  for (int i = 0; i < diagSize; i++) {
-     for (int j = 0; j < diagSize; j++) {
-     matrixA[i * diagSize + j] = dataA[i][j];
-	   }
-  }
+   for (int i = 0; i < diagSize; i++) {
+      for (int j = 0; j < diagSize; j++) {
+         matrixA[i * diagSize + j] = dataA[i][j];
+      }
+   }
 }
