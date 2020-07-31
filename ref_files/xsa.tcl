a90af414b9e1a8ca078a318595f803b621624e63
gewuek <jason.wu@xilinx.com>
2020-07-31 14:15:15 +0800

    Update xsa.tcl

--- ref_files/xsa.tcl
+++ ref_files/xsa.tcl
@@ -30,6 +30,3 @@ get_property platform.design_intent.datacenter [current_project]
 set_property platform.default_output_type "sd_card" [current_project]
 
 get_property platform.default_output_type [current_project]
-
-# Add the platform property to use dynamic_postlink.tcl during the v++ link
-set_property platform.post_sys_link_tcl_hook ./dynamic_postlink.tcl [current_project]
