7102a38b3c7a0d5b02039c51ea999b0ee29be131
ThomasXilinx <tbollaer@xilinx.com>
2020-08-18 11:43:51 -0700

    Improvements to the Vitis 101 tutorial

--- Getting_Started/Vitis/example/src/host.cpp
+++ Getting_Started/Vitis/example/src/host.cpp
@@ -22,7 +22,90 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 **********/
 
-#include "host.hpp"
+#define CL_HPP_CL_1_2_DEFAULT_BUILD
+#define CL_HPP_TARGET_OPENCL_VERSION 120
+#define CL_HPP_MINIMUM_OPENCL_VERSION 120
+#define CL_HPP_ENABLE_PROGRAM_CONSTRUCTION_FROM_ARRAY_COMPATIBILITY 1
+#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
+
+//OCL_CHECK doesn't work if call has templatized function call
+#define OCL_CHECK(error,call)                                       \
+    call;                                                           \
+    if (error != CL_SUCCESS) {                                      \
+      printf("%s:%d Error calling " #call ", error code is: %d\n",  \
+              __FILE__,__LINE__, error);                            \
+      exit(EXIT_FAILURE);                                           \
+    }
+#define DATA_SIZE 4096
+
+#include <vector>
+#include <unistd.h>
+#include <iostream>
+#include <fstream>
+#include <CL/cl2.hpp>
+
+template <typename T>
+struct aligned_allocator
+{
+  using value_type = T;
+  T* allocate(std::size_t num) {
+    void* ptr = nullptr;
+    if (posix_memalign(&ptr,4096,num*sizeof(T)))
+      throw std::bad_alloc();
+    return reinterpret_cast<T*>(ptr);
+  }
+  void deallocate(T* p, std::size_t num) {
+    free(p);
+  }
+};
+
+std::vector<cl::Device> get_xilinx_devices() {
+
+    size_t i;
+    cl_int err;
+    std::vector<cl::Platform> platforms;
+    const std::string vendor_name = "Xilinx";
+    OCL_CHECK(err, err = cl::Platform::get(&platforms));
+    cl::Platform platform;
+    for (i  = 0 ; i < platforms.size(); i++){
+        platform = platforms[i];
+        OCL_CHECK(err, std::string platformName = platform.getInfo<CL_PLATFORM_NAME>(&err));
+        if (platformName == vendor_name){
+            std::cout << "Found Platform" << std::endl;
+            std::cout << "Platform Name: " << platformName.c_str() << std::endl;
+            break;
+        }
+    }
+    if (i == platforms.size()) {
+        std::cout << "Error: Failed to find Xilinx platform" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+   
+    //Getting ACCELERATOR Devices and selecting 1st such device 
+    std::vector<cl::Device> devices;
+    OCL_CHECK(err, err = platform.getDevices(CL_DEVICE_TYPE_ACCELERATOR, &devices));
+    return devices;
+}
+   
+char* read_binary_file(const std::string &xclbin_file_name, unsigned &nb) 
+{
+    std::cout << "INFO: Reading " << xclbin_file_name << std::endl;
+
+    if(access(xclbin_file_name.c_str(), R_OK) != 0) {
+        printf("ERROR: %s xclbin not available please build\n", xclbin_file_name.c_str());
+        exit(EXIT_FAILURE);
+    }
+    //Loading XCL Bin into char buffer 
+    std::cout << "Loading: '" << xclbin_file_name.c_str() << "'\n";
+    std::ifstream bin_file(xclbin_file_name.c_str(), std::ifstream::binary);
+    bin_file.seekg (0, bin_file.end);
+    nb = bin_file.tellg();
+    bin_file.seekg (0, bin_file.beg);
+    char *buf = new char [nb];
+    bin_file.read(buf, nb);
+    return buf;
+}
+
 
 int main(int argc, char** argv)
 {
@@ -31,35 +114,34 @@ int main(int argc, char** argv)
 // ------------------------------------------------------------------------------------ 
     if (argc != 2) {
         std::cout << "Usage: " << argv[0] << " <XCLBIN File>" << std::endl;
-		return EXIT_FAILURE;
-	}
+        return EXIT_FAILURE;
+    }
 
 // ------------------------------------------------------------------------------------
-// Initialize local variables 
+// Section 1: Allocate and initialize host memory 
 // ------------------------------------------------------------------------------------ 
     std::string binaryFile = argv[1];
     unsigned fileBufSize;
     int size = DATA_SIZE;
     size_t vector_size_bytes = sizeof(int) * DATA_SIZE;
-    std::vector<int,aligned_allocator<int>> source_in1(DATA_SIZE);
-    std::vector<int,aligned_allocator<int>> source_in2(DATA_SIZE);
-    std::vector<int,aligned_allocator<int>> source_hw_results(DATA_SIZE);
-    std::vector<int,aligned_allocator<int>> source_sw_results(DATA_SIZE);
+    std::vector<int,aligned_allocator<int>> in1(DATA_SIZE);
+    std::vector<int,aligned_allocator<int>> in2(DATA_SIZE);
+    std::vector<int,aligned_allocator<int>> out(DATA_SIZE);
+    std::vector<int,aligned_allocator<int>> ref(DATA_SIZE);
 
     // Create the test data 
     for(int i = 0 ; i < DATA_SIZE ; i++){
-        source_in1[i] = rand() % DATA_SIZE;
-        source_in2[i] = rand() % DATA_SIZE;
-        source_sw_results[i] = source_in1[i] + source_in2[i];
-        source_hw_results[i] = 0;
+        in1[i] = rand() % DATA_SIZE;
+        in2[i] = rand() % DATA_SIZE;
+        ref[i] = in1[i] + in2[i];
+        out[i] = 0;
     }
 
-	
 // ------------------------------------------------------------------------------------
-// Initialize the OpenCL environment 
-// ------------------------------------------------------------------------------------	
+// Section 2: Initialize the OpenCL environment 
+// ------------------------------------------------------------------------------------ 
     cl_int err;
-    std::vector<cl::Device> devices = get_devices("Xilinx");
+    std::vector<cl::Device> devices = get_xilinx_devices();
     devices.resize(1);
     cl::Device device = devices[0];
     OCL_CHECK(err, cl::Context context(device, NULL, NULL, NULL, &err));
@@ -70,43 +152,40 @@ int main(int argc, char** argv)
     OCL_CHECK(err, cl::Kernel krnl_vector_add(program,"vadd", &err));
 
 // ------------------------------------------------------------------------------------
-// Create buffers
+// Section 3: Create buffers
 // ------------------------------------------------------------------------------------
-    OCL_CHECK(err, cl::Buffer buffer_in1(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, vector_size_bytes, source_in1.data(), &err));
-    OCL_CHECK(err, cl::Buffer buffer_in2(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, vector_size_bytes, source_in2.data(), &err));
-    OCL_CHECK(err, cl::Buffer buffer_out(context, CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY,vector_size_bytes, source_hw_results.data(), &err));
+    OCL_CHECK(err, cl::Buffer buffer_in1(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, vector_size_bytes, in1.data(), &err));
+    OCL_CHECK(err, cl::Buffer buffer_in2(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, vector_size_bytes, in2.data(), &err));
+    OCL_CHECK(err, cl::Buffer buffer_out(context, CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY,vector_size_bytes, out.data(), &err));
 
 // ------------------------------------------------------------------------------------
-// Run the kernel
+// Section 4: Run the kernel
 // ------------------------------------------------------------------------------------
     OCL_CHECK(err, err = krnl_vector_add.setArg(0, buffer_in1));
     OCL_CHECK(err, err = krnl_vector_add.setArg(1, buffer_in2));
     OCL_CHECK(err, err = krnl_vector_add.setArg(2, buffer_out));
     OCL_CHECK(err, err = krnl_vector_add.setArg(3, size));
 
-    OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_in1, buffer_in2},0/* 0 means from host*/));	
+    OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_in1, buffer_in2},0/* 0 means from host*/)); 
     OCL_CHECK(err, err = q.enqueueTask(krnl_vector_add));
     OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_out},CL_MIGRATE_MEM_OBJECT_HOST));
 
     // Wait for all scheduled operations to finish
     q.finish();
-	
-
+    
 // ------------------------------------------------------------------------------------
-// Check Results and Release Allocated Resources
+// Section 5: Check Results and Release Allocated Resources
 // ------------------------------------------------------------------------------------
     bool match = true;
     for (int i = 0 ; i < DATA_SIZE ; i++){
-        if (source_hw_results[i] != source_sw_results[i]){
+        if (out[i] != ref[i]){
             std::cout << "Error: Result mismatch" << std::endl;
-            std::cout << "i = " << i << " CPU result = " << source_sw_results[i]
-                << " Device result = " << source_hw_results[i] << std::endl;
+            std::cout << "i = " << i << " CPU result = " << ref[i] << " Device result = " << out[i] << std::endl;
             match = false;
             break;
         }
     }
-	
-
+    
     delete[] fileBuf;
 
     std::cout << "TEST " << (match ? "PASSED" : "FAILED") << std::endl; 
