d1110f316144bf7a5ff3b9a18c3c35d6bb1414e4
ThomasXilinx <tbollaer@xilinx.com>
2020-08-19 16:15:33 -0700

    Minor fixes and improvements
    Former-commit-id: c4b0d820cce0e0d4093176b756f11b6cf82575d4

--- Getting_Started/Vitis/example/src/host.cpp
+++ Getting_Started/Vitis/example/src/host.cpp
@@ -28,6 +28,8 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define CL_HPP_ENABLE_PROGRAM_CONSTRUCTION_FROM_ARRAY_COMPATIBILITY 1
 #define CL_USE_DEPRECATED_OPENCL_1_2_APIS
 
+#define DATA_SIZE 4096
+
 #include <vector>
 #include <unistd.h>
 #include <iostream>
@@ -37,9 +39,6 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 // Forward declaration of utility functions included at the end of this file
 std::vector<cl::Device> get_xilinx_devices();
 char* read_binary_file(const std::string &xclbin_file_name, unsigned &nb);
-
-
-#define DATA_SIZE 4096
 
 // ------------------------------------------------------------------------------------
 // Main program
@@ -127,7 +126,7 @@ int main(int argc, char** argv)
 
 
 // ------------------------------------------------------------------------------------
-// Source code for utility functions
+// Utility functions
 // ------------------------------------------------------------------------------------
 std::vector<cl::Device> get_xilinx_devices() 
 {
