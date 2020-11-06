4bf821e1675bacd072a113e451bf4a8bf4a1ac8e
Rob Armstrong <robert.armstrong-jr@xilinx.com>
2020-11-05 16:23:04 -0800

    Add edge to cloud migration example
    Former-commit-id: 7d13f98bd6f892ce202a8f31526015941c6ab660

---
+++ Machine_Learning/Feature_Tutorials/03-edge-to-cloud/files/image_input_fn.py
@@ -0,0 +1,32 @@
+'''
+ Copyright 2020 Xilinx Inc.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+'''
+
+'''
+Image processing for calibration
+Author: Mark Harvey
+'''
+
+import os
+from preprocess import preprocess
+
+
+calib_image_list = './build/quantize/calib_list.txt'
+calib_batch_size = 20
+def calib_input(iter):
+  images = []
+  line = open(calib_image_list).readlines()
+  for index in range(0, calib_batch_size):
+    curline = line[iter * calib_batch_size + index]
+    calib_image_name = curline.strip()
+    images.append(preprocess(calib_image_name))
+  return {"input_1": images}
