b01ca295dcc62d5f7f872610c98ac0e4d05c4aa3
Dachang Li <dachang@xsjswmtech03.xilinx.com>
2020-11-11 23:53:43 -0800

    update to 2020.2
    Former-commit-id: aafbadb4786971f54961b2fc776146d23c829ca5 [formerly 159b7f595c93b084a4a28c95f76025551ddf836a]
    Former-commit-id: cd4378956db7ca8b4b8a48c024ca7aaf0a88d31e
    Former-commit-id: 6c64efa5cdee4dceaae5610e71f6805bbd32157d

---
+++ Machine_Learning/Introduction/03-Basic/Module_7/build/HLS_V4l2s/CMakeFiles/hls_v4l2s.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hls_v4l2s.dir/src/hlsV4l2Access.cpp.o"
+  "CMakeFiles/hls_v4l2s.dir/src/hlsV4l2Capture.cpp.o"
+  "CMakeFiles/hls_v4l2s.dir/src/hlsV4l2Device.cpp.o"
+  "CMakeFiles/hls_v4l2s.dir/src/hlsV4l2MmapDevice.cpp.o"
+  "CMakeFiles/hls_v4l2s.dir/src/hlsV4l2Output.cpp.o"
+  "CMakeFiles/hls_v4l2s.dir/src/hlsV4l2ReadWriteDevice.cpp.o"
+  "CMakeFiles/hls_v4l2s.dir/src/xcl2.cpp.o"
+  "libhls_v4l2s.pdb"
+  "libhls_v4l2s.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hls_v4l2s.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
