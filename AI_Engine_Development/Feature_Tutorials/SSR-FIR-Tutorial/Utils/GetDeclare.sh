9c13efab2f10c6fa31dfa1061ba5a553ae6b7c4f
Aoibheann McCloskey <aoibheann.mccloskey@xilinx.com>
2020-12-11 16:42:15 +0000

    Added SSR FIR Tutorial
    Former-commit-id: 922cafcab1e6aba6b5bd17fd1feb95c0cfd23399 [formerly c784587cf29a2aecb22ca1ba0579021f3356137b]
    Former-commit-id: 4f2c3beb279d26f9a09993656ae6783af3ae3e67
    Former-commit-id: 879d4a8ceab20efcfa73cbe75e83b994f15e20d4

---
+++ AI_Engine_Development/Feature_Tutorials/SSR-FIR-Tutorial/Utils/GetDeclare.sh
@@ -0,0 +1,33 @@
+#
+# Copyright 2020 Xilinx, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#!/bin/bash
+
+
+row=0
+col=0
+for row in {0..7}
+do
+  echo " "
+  echo "Row ${row}"
+for col in {23..30}
+do
+  #echo "Tile $col $row"
+  File="${col}_${row}/src/${col}_${row}.cc"
+  cat $File | grep DoubleStream::
+done
+done
+
+ #cat $1/src/$1.cc | grep DoubleStream::
