8b07b33fe152c6bdf93c11b8802277934b630857
Ricky Su <rickys@xilinx.com>
2021-05-07 20:04:30 +0800

    pfm: update step3 to simplify platform package steps and scripts

--- Vitis_Platform_Creation/Introduction/03_Edge_VCK190/ref_files/step3_pfm/xsct_create_pfm.tcl
+++ Vitis_Platform_Creation/Introduction/03_Edge_VCK190/ref_files/step3_pfm/xsct_create_pfm.tcl
@@ -12,43 +12,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-setws ./build
+# Varoables
+set OUTPUT platform_repo
+
 set platform_name [lindex $argv 0]
 puts "The platform name is \"$platform_name\"" 
 
 set xsa_path [lindex $argv 1]
 puts "The xsa path is \"$xsa_path\"" 
 
-set OUTPUT platform_repo
-set SW_COMP platform_repo/tmp/sw_components/
+# XSCT commands
+setws ./build
 
 platform create -name $platform_name -desc "A custom platform VCK190 platform" -hw $xsa_path/$platform_name.xsa -out ./$OUTPUT -no-boot-bsp 
 
 # AIE domain
 domain create -name aiengine -os aie_runtime -proc ai_engine
-# AIE domain emulation
-# domain config -pmcqemu-args ./qemu/aie/pmc_args.txt
-# domain config -qemu-args ./qemu/aie/qemu_args.txt
-# domain config -qemu-data ./boot
+
 # Linux domain
-domain create -name xrt -proc psv_cortexa72 -os linux -arch {64-bit} -runtime {ocl} -sd-dir {./image}  -bootmode {sd}
+domain create -name xrt -proc psv_cortexa72 -os linux -arch {64-bit} -runtime {ocl} -sd-dir {./sd_dir}  -bootmode {sd}
 domain active xrt
 domain config -boot {../step2_petalinux/build/petalinux/images/linux}
 domain config -generate-bif
-# Linux domain emulation
-# domain config -pmcqemu-args ./qemu/lnx/pmc_args.txt
-# domain config -qemu-args ./qemu/lnx/qemu_args.txt
-# domain config -qemu-data ./boot
 
-
-# Standalone Domain
-#domain create -name {standalone} -display-name {standalone_domain} -os {standalone} -proc {psv_cortexa72_0} -runtime {cpp} -arch {64-bit} -support-app {hello_world}
-
+# Generate Platform
 platform write
 platform generate
-
-
-
-
-
-
