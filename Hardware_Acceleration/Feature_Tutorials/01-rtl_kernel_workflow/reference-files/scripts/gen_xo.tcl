a34e6f52f2bd97e443f953dcb776cc7fec59a811
Randy Hartgrove <randy.hartgrove@xilinx.com>
2021-10-12 22:05:04 -0700

    2021.2 next (#39)
    add description.json files and restrucure 07-host-code-opt

--- Hardware_Acceleration/Feature_Tutorials/01-rtl_kernel_workflow/reference-files/scripts/gen_xo.tcl
+++ Hardware_Acceleration/Feature_Tutorials/01-rtl_kernel_workflow/reference-files/scripts/gen_xo.tcl
@@ -51,4 +51,4 @@ if {[file exists "${xoname}"]} {
     file delete -force "${xoname}"
 }
 
-package_xo -xo_path ${xoname} -kernel_name Vadd_A_B -ip_directory ./packaged_kernel_${suffix} -kernel_xml ./src/xml/kernel.xml
+package_xo -xo_path ${xoname} -kernel_name Vadd_A_B -ip_directory ./packaged_kernel_${suffix} -kernel_xml ./src/xml/kernel.xml -kernel_files ./src/c-model/Vadd_A_B.cpp
