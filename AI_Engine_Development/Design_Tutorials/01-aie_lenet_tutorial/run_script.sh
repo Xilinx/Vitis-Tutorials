4bc0a98f5dc78e76f3186bf55a5972beabf451f7
Jithin Pillai <jithin.pillai@xilinx.com>
2022-03-02 12:48:16 +0530

    AIE LeNET: Fixed timeout issues

--- AI_Engine_Development/Design_Tutorials/01-aie_lenet_tutorial/run_script.sh
+++ AI_Engine_Development/Design_Tutorials/01-aie_lenet_tutorial/run_script.sh
@@ -13,10 +13,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+echo ""
 date
+echo ""
+SECONDS=0
 
 export XLC_EMULATION_MODE=hw_emu
 export XILINX_XRT=/usr
 ./lenet_xrt.elf a.xclbin
-
+return_code=$?
+if [ $return_code -ne 0 ]; then
+        echo "ERROR: Embedded host run failed, RC=$return_code"
+else
+        echo "INFO: TEST PASSED, RC=0"
+fi
+duration=$SECONDS
+echo ""
+echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
+echo ""
 date
+echo ""
+echo "INFO: Embedded host run completed."
+echo ""
+exit $return_code
