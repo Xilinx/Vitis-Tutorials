3e5ffcf205ea7e3f72bdeafd787bd6784b0694ce
Sven Wu <sven@xilinx.com>
2022-11-21 11:44:57 +0800

    PFM/F/petalinux: add hw run support for regression

---
+++ Vitis_Platform_Creation/Feature_Tutorials/02_petalinux_customization/ref_files/zcu104/convert_to_fixed_xsa.tcl
@@ -0,0 +1,8 @@
+open_hw_platform hw.xsa
+set_property platform.extensible false [current_project]
+
+
+launch_runs impl_1 -to_step write_bitstream -jobs 32
+wait_on_run impl_1
+generate_target all [get_files [current_bd_design].bd]
+write_hw_platform -fixed -include_bit -force -file  xsa_file_for_petalinux.xsa
---
+++ Vitis_Platform_Creation/Feature_Tutorials/02_petalinux_customization/ref_files/zcu104/run_app.sh
@@ -0,0 +1,11 @@
+export PATH=$PATH:/sbin
+export XILINX_XRT=/usr
+export XILINX_VITIS=$PWD
+devmem 0x40000000
+return_code=$?
+echo "$return_code"
+if [ $return_code -ne 0 ]; then
+echo "ERROR: host run failed, RC=$return_code"
+else
+echo "INFO: host run completed."
+fi
