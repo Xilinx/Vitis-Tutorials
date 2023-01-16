aa303a680433fe37c1a07cecaf35617e8392af9b
arndtc <arndtc@gmail.com>
2023-01-16 00:21:53 -0700

    Print error if the files are not opened. (#358)
    Print error if the files are not opened.

--- Hardware_Acceleration/Design_Tutorials/06-cholesky-accel/03-Algorithm_Acceleration/docs/hls_tb/test_hls.cpp
+++ Hardware_Acceleration/Design_Tutorials/06-cholesky-accel/03-Algorithm_Acceleration/docs/hls_tb/test_hls.cpp
@@ -39,21 +39,33 @@ int main(int argc, const char* argv[]) {
    double tmp;
 
    fp=fopen("tb_data/matrix_input.dat","r");
-   for (int i = 0; i < dataAN*dataAM; i++){
-      fscanf(fp, "%lf", &tmp);
-      dataA[i] = tmp;
-   }
-   fclose(fp);
-
-   fp=fopen("tb_data/golden_result.dat","r");
-   for (int i = 0; i < dataAN; i++){
-      for (int j = 0; j < dataAM; j++){
-         fscanf(fp, "%lf", &tmp);
-         dataC[i][j] = tmp;
+      if (fp!=NULL) {
+         for (int i = 0; i < dataAN*dataAM; i++){
+            fscanf(fp, "%lf", &tmp);
+            dataA[i] = tmp;
+         }
+         fclose(fp);
       }
-   }
-   fclose(fp);
-
+      else
+      {
+  	      printf("ERROR Opening File Pointer\n");
+	      exit(-1);
+      }
+   fp=fopen("tb_data/golden_result.dat","r");
+      if (fp!=NULL) {
+         for (int i = 0; i < dataAN; i++){
+            for (int j = 0; j < dataAM; j++){
+               fscanf(fp, "%lf", &tmp);
+               dataC[i][j] = tmp;
+            }
+         }
+         fclose(fp);
+      }
+      else
+      {
+  	      printf("ERROR Opening File Pointer\n");
+	      exit(-1);
+      }
    cholesky_kernel(dataAN, dataA);
 
    // Calculate err between dataA and dataC
