1bc5f8c21592dffcfb930d3ea801d0b491fb5854
Suruthi G <suruthi.g@xilinx.com>
2023-01-28 14:16:41 +0530

    AIE/D/06-fft2d: Updated README and env var in Makefile for regression
    Update DSP library links mentioned in FFT Tutorial readme . Previously it pointed to github.io, I've changed them to point DSP library docs.
    Updated JSON file for regression: removed CXX flags to avoid overwritten of predefined CXX flags
    Updated Makefile with Image path as COMMON_IMAGE_VERSAL for packing rootfs and image

--- AI_Engine_Development/Design_Tutorials/06-fft2d_AIEvsHLS/HLS/sample_env_setup.sh
+++ AI_Engine_Development/Design_Tutorials/06-fft2d_AIEvsHLS/HLS/sample_env_setup.sh
@@ -14,39 +14,41 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-export PLATFORM_REPO_PATHS=<YOUR-PLATFORMS-DIRECTORY>
-export XILNX_TOOLS_LOCATION=<Path to Vitis Build - Directory>/Vitis/2022.1
-export XLNX_VERSAL=<Path to xilinx-versal-common-v2022.1 - Directory>
-
-# Optionally Setup XRT_ROOT, pointing to XRT RPMs,
-# if not set it will automatically be excluded from v++ packaging...
-#export XRT_ROOT=<Path to XRT RPMs - Directory>
-
-# Setup SDKTARGETSYSROOT and CXX...
-source $XILINX_VERSAL/environment-setup-cortexa72-cortexa53-xilinx-linux
-
-# Source XRT, VITIS, and Aietools...
-source $XILNX_TOOLS_LOCATION/settings64.sh
+# =======================================================
+# Set Platform ,Vitis and Versal Image repo
+# =======================================================
+export PLATFORM_REPO_PATHS= <YOUR-2022.2-PLATFORM-DIRECTORY>
+export XILINX_VITIS = <YOUR-2022.2-VITIS-DIRECTORY>/2022.2
+export COMMON_IMAGE_VERSAL=<YOUR-XILINX-VERSAL-COMMON-V2022.2-DIRECTORY>
+# ====================================================
+# Source Versal Image ,Vitis and Aietools
+# ====================================================
+# Run the below command to setup environment and CXX
+source $COMMON_IMAGE_VERSAL/environment-setup-cortexa72-cortexa53-xilinx-linux
+source $XILINX_VITIS/settings64.sh
 
 # =========================================================
 # Platform Selection...
 # =========================================================
-tgt_plat=xilinx_vck190_base_202210_1
+tgt_plat=xilinx_vck190_base_202220_1
 export PLATFORM=$PLATFORM_REPO_PATHS/$tgt_plat/$tgt_plat\.xpfm
-
+# ==========================================================
+# Validating Tool Installation
+# ==========================================================
 echo ""
 echo "Aiecompiler:"
 which aiecompiler
-
 echo ""
 echo "Vivado:"
 which vivado
-
 echo ""
 echo "Vitis:"
 which vitis
-
 echo ""
 echo "Vitis HLS:"
 which vitis_hls
+echo ""
+echo ""
+echo "DSPLIBS"
+echo "$DSPLIB_VITIS"
 echo ""
