bcb4165178881af0e1fee61fb023e066991b8d4f
Harika BALUSU <harika.devi.krishna.priya.balusu@xilinx.com>
2023-05-09 12:53:05 +0530

    AIE/F/17-RTL-IP: change license header to MIT

--- AI_Engine_Development/Feature_Tutorials/17-RTL-IP-with-AIE-Engines/pl_kernels/hdl/vhdl/polar_clip_rotation_cos_lut.vhd
+++ AI_Engine_Development/Feature_Tutorials/17-RTL-IP-with-AIE-Engines/pl_kernels/hdl/vhdl/polar_clip_rotation_cos_lut.vhd
@@ -1,20 +1,9 @@
--- © Copyright 2020 Xilinx, Inc.
 
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
-
---     http://www.apache.org/licenses/LICENSE-2.0
-
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
+--Copyright (C) 2023, Advanced Micro Devices, Inc. All rights reserved.
+--SPDX-License-Identifier: X11
 
 -- ==============================================================
 -- Vitis HLS - High-Level Synthesis from C, C++ and OpenCL v2020.2 (64-bit)
--- Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 -- ==============================================================
 library ieee; 
 use ieee.std_logic_1164.all; 
