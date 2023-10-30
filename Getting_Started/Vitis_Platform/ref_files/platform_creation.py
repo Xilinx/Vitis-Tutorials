beb9e650fc82bd4aa29f3a245cbfa185b2089f9e
Sven Wu <sven.wu@amd.com>
2023-10-30 17:16:13 +0800

    PFM/G/Vtis_platform: update to 23.2

--- Getting_Started/Vitis_Platform/ref_files/platform_creation.py
+++ Getting_Started/Vitis_Platform/ref_files/platform_creation.py
@@ -5,32 +5,29 @@
 
 # 2023-04-02T21:02:13.258137
 import vitis
+import argparse
 import os
 
 print("Platform generation")
-platform_name = "vck190_custom"
-xsa_name="vck190_custom.xsa"
-xsa_path=os.path.join(os.getcwd(),"build/vivado",xsa_name)
-print(xsa_path)
-boot="boot"
-boot=os.path.join(os.getcwd(),boot)
-sd_dir="sd_dir"
-sd_dir=os.path.join(os.getcwd(),sd_dir)
+parser = argparse.ArgumentParser()
+parser.add_argument("--platform_name", type=str, dest="platform_name")
+parser.add_argument("--xsa_path", type=str, dest="xsa_path")
+parser.add_argument("--boot", type=str, dest="boot")
+parser.add_argument("--dtb", type=str, dest="dtb")
+args = parser.parse_args()
+platform_name=args.platform_name
+xsa_path=args.xsa_path
+dtb=args.dtb
+boot=args.boot
+print('args',args)
 client = vitis.create_client()
-print(boot)
-print(sd_dir)
 client.set_workspace(path=os.getcwd())
-
-
-platform = client.create_platform(name = platform_name, hw =xsa_path, os = "linux",cpu = "psv_cortexa72")
-
+platform = client.create_platform_component(name = platform_name, hw =xsa_path, os = "aie_runtime",cpu = "ai_engine")
 platform = client.get_platform_component(name=platform_name)
-domain = platform.get_domain(name="linux_psv_cortexa72")
-status = domain.update_name(new_name="xrt")
-status = domain.add_boot_dir(boot_dir=boot)
-
-status = domain.set_sd_dir(path=sd_dir)
-status = domain.add_qemu_data(data_dir=boot)
+domain = platform.add_domain(cpu = "psv_cortexa72",os = "linux",name = "xrt",display_name = "xrt")
+domain = platform.get_domain(name="xrt")
 status = domain.generate_bif() 
-status = platform.build()
+status = domain.add_boot_dir(path=boot)
+status = domain.set_dtb(path=dtb)
 
+status = platform.build()
