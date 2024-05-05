2d4a1a420a779a06fa626c92471818e5aac1237f
Ally Zhou <allyz@amd.com>
2024-05-05 23:43:56 +0800

    GS/Vitis_Libraries: update to comp.run for vhls flow

--- Getting_Started/Vitis_Libraries/project.py
+++ Getting_Started/Vitis_Libraries/project.py
@@ -1,26 +1,52 @@
+#!/usr/bin/env python3
 import vitis
 import os
+import time
 import shutil
+from datetime import datetime
 
 
+# Create names
+tmpdir = './tmp'
+date = datetime.now().strftime("%Y%m%d%I%M%S")
+workspace_name = 'workspace_'+date
+workspace = os.path.join(tmpdir, workspace_name)
+comp_name = "hls_component"
+cwd = os.getcwd()
+cfg_name = 'project.cfg'
+cfg_path = os.path.join(cwd, cfg_name)
+
+# Delete the workspace if already exists
+if (os.path.isdir(tmpdir)):
+    shutil.rmtree(tmpdir, ignore_errors=True)
+    print(f"Deleted workspaces in {tmpdir}")
+
+# Print names
+print(f"CWD is {cwd}")
+print(f"Workspace path is {workspace}")
+print(f"Creating component {comp_name}")
+print(f"Using config file {cfg_path}")
+
+# Create a client object
 client = vitis.create_client()
-workspace='./myProject'
 
-#Delete the workspace if already exists.
-if (os.path.isdir(workspace)):
-    shutil.rmtree(workspace)
-    print(f"Deleted workspace {workspace}")
-
+# Set Workspace
 client.set_workspace(workspace)
 
-cwd = os.getcwd()
-print(f"CWD is {cwd}")
+# Create HLS Component
+comp = client.create_hls_component(name=comp_name, cfg_file = cfg_path)
 
-cfg_path = os.path.join(cwd, 'project.cfg')
-print(f"Config file is {cfg_path}")
+# Execute the desired steps:
+comp.run('C_SIMULATION')
+comp.run('SYNTHESIS')
+comp.run('CO_SIMULATION')
+#comp.run('PACKAGE')
+#comp.run('IMPLEMENTATION')
 
-comp = client.create_hls_component(name="impulseTest", cfg_file = cfg_path)
-comp.execute(operation="C_SIMULATION")
-comp.execute(operation="SYNTHESIS")
-comp.execute(operation="CO_SIMULATION")
+# Clean up, delete the workspace
+if (os.path.isdir(tmpdir)):
+    shutil.rmtree(tmpdir, ignore_errors=True)
+    print(f"Deleted workspaces in {tmpdir}")
 
+# Close the client connection and terminate the vitis server
+vitis.dispose()
