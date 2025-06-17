831e0bbc2a2e8696aa2c6d12ba05b0795ea5ee34
Zhou, Ally <ally.zhou@amd.com>
2025-06-17 10:55:25 +0800

    Deprecation: Remove outdated tutorial

--- AI_Engine_Development/AIE/Feature_Tutorials/16-external-traffic-generator-aie/HW_Emulation/scripts/etg_matlab.sh
+++
@@ -1,3 +0,0 @@
-echo ${PWD}
-/tools/gensys/matlab/R2022a/bin/matlab -batch "scriptDir = fullfile(pwd, 'TrafficGenerator/MATLAB');addpath(scriptDir);scriptToRun = 'xtg_aie.m';run(scriptToRun);rmpath(scriptDir);"
-
