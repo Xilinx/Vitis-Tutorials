8428269d09984c02dba6e0375a9a3cb8f08adda6
Rane, Kunal <kunal.rane@amd.com>
2025-08-21 13:05:28 +0530

    PFM/F/01: Update streaming IP tutorial for 2025.1

--- Vitis_Platform_Creation/Feature_Tutorials/01_platform_creation_streaming_ip/ref_files/platform_creation.py
+++ Vitis_Platform_Creation/Feature_Tutorials/01_platform_creation_streaming_ip/ref_files/platform_creation.py
@@ -14,22 +14,29 @@ parser.add_argument("--platform_name", type=str, dest="platform_name")
 parser.add_argument("--xsa_path", type=str, dest="xsa_path")
 parser.add_argument("--xsa-emu_path", type=str, dest="emuxsa_path")
 parser.add_argument("--boot", type=str, dest="boot")
-parser.add_argument("--dtb", type=str, dest="dtb")
 args = parser.parse_args()
 platform_name=args.platform_name
 xsa_path=args.xsa_path
 emuxsa_path=args.emuxsa_path
-dtb=args.dtb
+TOP_DIR = os.getcwd()
+USER_DTSI = TOP_DIR + "/system-user.dtsi"
 boot=args.boot
 print('args',args)
 client = vitis.create_client()
-client.set_workspace(path=os.getcwd())
-platform = client.create_platform_component(name = platform_name, hw_design =xsa_path, os = "aie_runtime",cpu = "ai_engine",emu_design = emuxsa_path )
-platform = client.get_component(name=platform_name)
-domain = platform.add_domain(cpu = "psv_cortexa72",os = "linux",name = "xrt",display_name = "xrt")
-domain = platform.get_domain(name="xrt")
-status = domain.generate_bif() 
-status = domain.set_boot_dir(path=boot)
-status = domain.set_dtb(path=dtb)
+workspace_path = os.getcwd() + "/ws" 
+client.set_workspace(path=workspace_path)
+print(workspace_path)
+#advanced_options = client.create_advanced_options_dict(board_dtsi="versal-vck190-reva-x-ebm-02-reva",dt_zocl="1",dt_overlay="0", user_dtsi=USER_DTSI)
+advanced_options = client.create_advanced_options_dict(dt_zocl="1",dt_overlay="0", user_dtsi=USER_DTSI)
+platform = client.create_platform_component(name =platform_name ,hw_design = xsa_path,os = "linux",cpu = "psv_cortexa72",domain_name = "linux_psv_cortexa72",generate_dtb = True,advanced_options = advanced_options)
 
+platform = client.get_component(name=platform_name)
+domain = platform.add_domain(cpu = "ai_engine",os = "aie_runtime",name = "ai_eingine",display_name = "ai_eingine")
+
+domain = platform.get_domain(name="linux_psv_cortexa72")
+
+status = domain.update_name(new_name="xrt")
+
+status = domain.generate_bif()
+status = domain.set_boot_dir(path=boot)
 status = platform.build()
