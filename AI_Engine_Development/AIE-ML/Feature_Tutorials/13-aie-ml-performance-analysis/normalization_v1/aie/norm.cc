07b1255f28e9dc1c999334e2960ed5d427dd941f
Pettigrew, Mark <Mark.Pettigrew@amd.com>
2026-04-13 23:28:02 -0400

    AIE-ML/F/13-aie-ml-performance-analysis: Editorial review and compliance fixes

--- AI_Engine_Development/AIE-ML/Feature_Tutorials/13-aie-ml-performance-analysis/normalization_v1/aie/norm.cc
+++ AI_Engine_Development/AIE-ML/Feature_Tutorials/13-aie-ml-performance-analysis/normalization_v1/aie/norm.cc
@@ -1,7 +1,7 @@
 /*
-Copyright (C) 2023, Advanced Micro Devices, Inc. All rights reserved.
-SPDX-License-Identifier: MIT
-*/
+ * Copyright Advanced Micro Devices, Inc.
+ * SPDX-License-Identifier: MIT
+ */
 #include <aie_api/aie.hpp>
 #include <aie_api/aie_adf.hpp>
 using namespace adf;
--- AI_Engine_Development/AIE-ML/Feature_Tutorials/13-aie-ml-performance-analysis/normalization_v2/aie/graph.h
+++ AI_Engine_Development/AIE-ML/Feature_Tutorials/13-aie-ml-performance-analysis/normalization_v2/aie/graph.h
@@ -1,7 +1,7 @@
 /*
-Copyright (C) 2023, Advanced Micro Devices, Inc. All rights reserved.
-SPDX-License-Identifier: MIT
-*/
+ * Copyright Advanced Micro Devices, Inc.
+ * SPDX-License-Identifier: MIT
+ */
 #ifndef GRAPH_MODULE_H
 #define GRAPH_MODULE_H
 
